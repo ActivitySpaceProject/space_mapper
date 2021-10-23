290b7d302dd964fac83113223dc94daad4ffe8dd
Pablo Galve <pablogalve100@gmail.com>
2021-10-23 19:50:31 +0200

    Report an Issue Screen: User Interface (UI) finished

--- Space_Mapper/lib/ui/report_an_issue.dart
+++ Space_Mapper/lib/ui/report_an_issue.dart
@@ -4,35 +4,23 @@ import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 
 class ReportAnIssue extends StatelessWidget {
-  _launchGithubIssues() async {
-    const url = 'https://github.com/ActivitySpaceProject/space_mapper/issues';
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Report an Issue")),
-      body: reportIssueBody(),
+      appBar: AppBar(title: Text("Report an Issue/Feature Request")),
+      body: reportIssueBody(context),
     );
   }
 }
 
-Widget reportIssueBody() {
+Widget reportIssueBody(BuildContext context) {
   return Padding(
       padding: EdgeInsets.all(20.0),
       child: Column(
         children: [
-          Container(
-            padding: EdgeInsets.only(bottom: 15.0),
-            child: Text(
-              "Help us improve by either reporting an issue or requesting a useful feature.",
-              style: TextStyle(fontSize: 17.0),
-            ),
+          Text(
+            "Help us improve by either reporting an issue or requesting a useful feature.",
+            style: TextStyle(fontSize: 17.0),
           ),
           DisplayService(
               "Github",
@@ -41,10 +29,21 @@ Widget reportIssueBody() {
                 size: ReportAnIssueStyle.iconSize,
               )),
           Container(
-            //padding: EdgeInsets.all(10.0),
-            child: Text("Report issues on github to get the fastest solution."),
+            margin: EdgeInsets.only(bottom: 10.0),
+            child: Text(
+              "Report issues on github to get the fastest solution.",
+              style: TextStyle(fontSize: 17.0),
+            ),
           ),
-          Text("Here goes a button"),
+          CustomButtonWithUrl(
+              "Go to Github Issues",
+              "https://github.com/ActivitySpaceProject/space_mapper/issues",
+              MaterialStateProperty.all(Colors.lightBlue[100]),
+              context),
+          Container(
+            //Container only to add more margin
+            margin: EdgeInsets.only(bottom: 10.0),
+          ),
           DisplayService(
               "Email",
               Icon(
@@ -52,19 +51,36 @@ Widget reportIssueBody() {
                 size: ReportAnIssueStyle.iconSize,
               )),
           Container(
-            //padding: EdgeInsets.all(10.0),
-            child: Text("As an alternative, you can send us an email."),
-          ),
-          Text("Here goes a button"),
-          Text("Here goes a button")
+              margin: EdgeInsets.only(bottom: 10.0),
+              child: Text(
+                "As an alternative, you can send us an email.",
+                style: TextStyle(fontSize: 17.0),
+              )),
+          CustomButtonWithUrl(
+              "Report an issue by email",
+              "https://github.com/ActivitySpaceProject/space_mapper/issues",
+              MaterialStateProperty.all(Colors.red[100]),
+              context),
+          CustomButtonWithUrl(
+              "Request a feature by email",
+              "https://github.com/ActivitySpaceProject/space_mapper/issues",
+              MaterialStateProperty.all(Colors.lightBlue[100]),
+              context),
         ],
       ));
 }
 
-//Display
+_launchUrl(String url) async {
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
+
 Widget DisplayService(String name, Icon icon) {
   return Container(
-    margin: EdgeInsets.only(bottom: 20.0),
+    margin: EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 20.0),
     child: Row(
       children: [
         icon,
@@ -72,10 +88,31 @@ Widget DisplayService(String name, Icon icon) {
           margin: EdgeInsets.only(right: 25.0),
         ),
         Text(
-          "Github",
+          name,
           style: TextStyle(fontSize: 25.0),
         ),
       ],
     ),
   );
+}
+
+Widget CustomButtonWithUrl(String text, String openUrl,
+    MaterialStateProperty<Color?> backgroundColor, BuildContext context) {
+  return Container(
+      width: MediaQuery.of(context).size.width * 0.6,
+      child: TextButton(
+        style: ButtonStyle(
+            backgroundColor: backgroundColor,
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18.0),
+                    side: BorderSide(color: Colors.black)))),
+        onPressed: () {
+          _launchUrl(openUrl);
+        },
+        child: Text(
+          text,
+          style: TextStyle(color: Colors.black),
+        ),
+      ));
 }

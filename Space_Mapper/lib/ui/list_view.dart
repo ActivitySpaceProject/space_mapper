b1ecf111256f231a2b90b51f77f2cd230a517505
Pablo Galve <pablogalve100@gmail.com>
2021-10-26 00:12:52 +0200

    Make Location History more user-friendly fix #14

--- Space_Mapper/lib/ui/list_view.dart
+++ Space_Mapper/lib/ui/list_view.dart
@@ -1,6 +1,67 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_background_geolocation/flutter_background_geolocation.dart'
     as bg;
+import 'package:geocoding/geocoding.dart';
+
+Future<String?> getLocationData(
+    String dataType, double lat, double long) async {
+  try {
+    List<Placemark> placemarks = await placemarkFromCoordinates(
+      lat,
+      long,
+    );
+    switch (dataType) {
+      case "locality":
+        return placemarks[0].locality;
+      case "subAdministrativeArea":
+        return placemarks[0].subAdministrativeArea;
+      case "ISOCountry":
+        return placemarks[0].isoCountryCode;
+      default:
+        return "";
+    }
+  } catch (err) {}
+}
+
+Future<List<dynamic>>? buildLocationsList() async {
+  List locations = await bg.BackgroundGeolocation.locations;
+  List ret = [];
+
+  for (int i = 0; i < locations.length; ++i) {
+    String? locality = await getLocationData(
+        "locality",
+        locations[i]['coords']['latitude'],
+        locations[i]['coords']['longitude']);
+    String? administrativeArea = await getLocationData(
+        "subAdministrativeArea",
+        locations[i]['coords']['latitude'],
+        locations[i]['coords']['longitude']);
+    String? ISOCountry = await getLocationData(
+        "ISOCountry",
+        locations[i]['coords']['latitude'],
+        locations[i]['coords']['longitude']);
+    String timestamp = locations[i]['timestamp'];
+    String activity = locations[i]['activity']['type'];
+    num speed = locations[i]['coords']['speed'];
+    num altitude = locations[i]['coords']['altitude'];
+    var add = new displayLocation(locality!, administrativeArea!, ISOCountry!,
+        timestamp, activity, speed, altitude);
+    ret.add(add);
+  }
+  return ret;
+}
+
+class displayLocation {
+  displayLocation(this.locality, this.subAdministrativeArea, this.ISOCountry,
+      this.timestamp, this.activity, this.speed, this.altitude);
+  late String locality;
+  late String subAdministrativeArea;
+  late String ISOCountry;
+  late String timestamp;
+  late String activity;
+  late num speed;
+  late num altitude;
+}
 
 class STOListView extends StatelessWidget {
   @override
@@ -8,7 +69,7 @@ class STOListView extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(title: Text("Location History")),
         body: FutureBuilder<List>(
-          future: bg.BackgroundGeolocation.locations,
+          future: buildLocationsList(),
           builder: (context, snapshot) {
             if (snapshot.hasData) {
               List? data = snapshot.data;
@@ -25,12 +86,20 @@ class STOListView extends StatelessWidget {
     return ListView.builder(
         itemCount: data.length,
         itemBuilder: (context, index) {
-          var thisLocation = data[index];
+          displayLocation thisLocation = data[index];
           return _tile(
-              thisLocation['timestamp'] +
-                  " activity: " +
-                  thisLocation['activity']['type'].toString(),
-              thisLocation['coords'].toString(),
+              thisLocation.locality +
+                  ", " +
+                  thisLocation.subAdministrativeArea +
+                  ", " +
+                  thisLocation.ISOCountry,
+              thisLocation.timestamp +
+                  " Activity: " +
+                  thisLocation.activity +
+                  " Speed: " +
+                  thisLocation.speed.toString() +
+                  " Altitude: " +
+                  thisLocation.altitude.toString(),
               Icons.gps_fixed);
         });
   }

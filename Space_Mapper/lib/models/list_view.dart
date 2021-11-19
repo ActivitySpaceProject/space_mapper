b1de7564816015604e8ad1302c1060136d2bbaee
Pablo <eduinvestor100@gmail.com>
2021-11-19 13:36:28 +0100

    [Location History] Divide UI and logic in separate files

--- Space_Mapper/lib/models/list_view.dart
+++ Space_Mapper/lib/models/list_view.dart
@@ -1,9 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_background_geolocation/flutter_background_geolocation.dart'
     as bg;
+import 'package:geocoding/geocoding.dart';
 import '../app_localizations.dart';
 
 class CustomLocationsManager {
+  static Future<List<CustomLocation>> getLocations(int maxElements) async {
+    List<CustomLocation> customLocations = [];
+
+    // Make the current list to be empty. We want to fill it according to our custom parameters
+    //CustomLocationsManager.removeAllCustomLocations();
+
+    // Get a list of locations from the flutter_background_geolocation plugin database
+    List recordedLocations = await bg.BackgroundGeolocation.locations;
+
+    // n is the minimum value of either the specified maximum amount of elements (maxElements), or the current size of recordedLocations
+    int n;
+    if (maxElements <= recordedLocations.length)
+      n = maxElements;
+    else
+      n = recordedLocations.length;
+
+    // Fill the custom locations list, to display beautiful tiles instead of json data
+    for (int i = 0; i < n; ++i) {
+      // Check if there's already a location with the same UUID
+      for (int j = customLocations.length - 1; j >= 0; --j) {
+        if (recordedLocations[i]['uuid'] == customLocations[j].getUUID())
+          continue; //CustomLocationsManager.customLocations[j].getUUID()) continue;
+      }
+      // Match not found, we add the location
+      CustomLocation newLocation = await CustomLocationsManager.createCustomLocation(
+          recordedLocations[i]
+          );
+      customLocations.add(newLocation);
+    }
+    return customLocations;
+  }
+
   /// Makes timestamp readable by a human
   static String formatTimestamp(String timestamp) {
     //2021-10-25T21:25:08.210Z <- This is the original format
@@ -17,6 +50,51 @@ class CustomLocationsManager {
       else if (timestamp[i] == ".") break;
     }
     return result;
+  }
+
+  static Future<CustomLocation> createCustomLocation(
+      var recordedLocation) async {
+    CustomLocation location = new CustomLocation();  
+
+    //Save data from flutter_background_geolocation library
+    location.setUUID(recordedLocation['uuid']);
+    location.setTimestamp(recordedLocation['timestamp']);
+    location.setActivity(recordedLocation['activity']['type']);
+    location.setSpeed(recordedLocation['coords']['speed'],
+        recordedLocation['coords']['speed_accuracy']);
+    location.setAltitude(recordedLocation['coords']['altitude'],
+        recordedLocation['coords']['altitude_accuracy']);
+
+    Placemark? placemark = await getLocationData(recordedLocation['coords']['latitude'],
+                recordedLocation['coords']['longitude']);
+
+    //Add our custom data
+    if (placemark != null) {
+      String? locality = placemark.locality;
+      String? subAdminArea = placemark.subAdministrativeArea;
+      String? street = placemark.street;
+      if (street != null) street += ", ${placemark.name}";
+      // ignore: non_constant_identifier_names
+      String? ISO = placemark.isoCountryCode;
+
+      location.setLocality(locality!);
+      location.setSubAdministrativeArea(subAdminArea!);
+      location.setStreet(street!);
+      location.setISOCountry(ISO!);
+    }
+    return location;
+  }
+  ///Get data such as city, province, postal code, street name, country...
+  static Future<Placemark?> getLocationData(double lat, double long) async {
+    try {
+      List<Placemark> placemarks = await placemarkFromCoordinates(
+        lat,
+        long,
+      );
+      return placemarks[0];
+    } catch (err) {
+      return null;
+    }
   }
 }
 
@@ -62,6 +140,8 @@ class CustomLocation {
       /// We need this checker to ensure that the user doesn't send the delete request twice, causing an exception
       _toDelete = true;
       await bg.BackgroundGeolocation.destroyLocation(this.getUUID());
+      //CustomLocationsManager.customLocations
+      //    .removeWhere((element) => element.getUUID() == this.getUUID());
     }
   }
 
--- Space_Mapper/lib/ui/list_view.dart
+++ Space_Mapper/lib/ui/list_view.dart
@@ -1,55 +1,6 @@
 import '../app_localizations.dart';
 import '../models/list_view.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_background_geolocation/flutter_background_geolocation.dart'
-    as bg;
-import 'package:geocoding/geocoding.dart';
-
-///Get data such as city, province, postal code, street name, country...
-Future<Placemark?> getLocationData(double lat, double long) async {
-  try {
-    List<Placemark> placemarks = await placemarkFromCoordinates(
-      lat,
-      long,
-    );
-    return placemarks[0];
-  } catch (err) {
-    return null;
-  }
-}
-
-CustomLocation createCustomLocation(
-    var recordedLocation, Placemark? placemark) {
-  CustomLocation location = new CustomLocation();
-  //Add location to list
-  //CustomLocationsManager.customLocations.add(location);
-
-  //Save data from flutter_background_geolocation library
-  location.setUUID(recordedLocation['uuid']);
-  location.setTimestamp(recordedLocation['timestamp']);
-  location.setActivity(recordedLocation['activity']['type']);
-  location.setSpeed(recordedLocation['coords']['speed'],
-      recordedLocation['coords']['speed_accuracy']);
-  location.setAltitude(recordedLocation['coords']['altitude'],
-      recordedLocation['coords']['altitude_accuracy']);
-
-  //Add our custom data
-  if (placemark != null) {
-    String? locality = placemark.locality;
-    String? subAdminArea = placemark.subAdministrativeArea;
-    String? street = placemark.street;
-    if (street != null) street += ", ${placemark.name}";
-    // ignore: non_constant_identifier_names
-    String? ISO = placemark.isoCountryCode;
-
-    location.setLocality(locality!);
-    location.setSubAdministrativeArea(subAdminArea!);
-    location.setStreet(street!);
-    location.setISOCountry(ISO!);
-  }
-
-  return location;
-}
 
 class STOListView extends StatefulWidget {
   const STOListView({Key? key}) : super(key: key);
@@ -58,46 +9,11 @@ class STOListView extends StatefulWidget {
   _STOListViewState createState() => _STOListViewState();
 }
 
-class _STOListViewState extends State<STOListView> {
-  final int maxElements = 25; // Maximum amount of elements displayed on screen
-
-  Future<List<CustomLocation>> recalculateLocations() async {
-    List<CustomLocation> customLocations = [];
-
-    // Make the current list to be empty. We want to fill it according to our custom parameters
-    //CustomLocationsManager.removeAllCustomLocations();
-
-    // Get a list of locations from the flutter_background_geolocation plugin database
-    List recordedLocations = await bg.BackgroundGeolocation.locations;
-
-    // n is the minimum value of either the specified maximum amount of elements (maxElements), or the current size of recordedLocations
-    int n;
-    if (maxElements <= recordedLocations.length)
-      n = maxElements;
-    else
-      n = recordedLocations.length;
-
-    // Fill the custom locations list, to display beautiful tiles instead of json data
-    for (int i = 0; i < n; ++i) {
-      // Check if there's already a location with the same UUID
-      for (int j = customLocations.length - 1; j >= 0; --j) {
-        if (recordedLocations[i]['uuid'] == customLocations[j].getUUID())
-          continue; //CustomLocationsManager.customLocations[j].getUUID()) continue;
-      }
-      // Match not found, we add the location
-      CustomLocation newLocation = createCustomLocation(
-          recordedLocations[i],
-          await getLocationData(recordedLocations[i]['coords']['latitude'],
-              recordedLocations[i]['coords']['longitude']));
-      customLocations.add(newLocation);
-    }
-    return customLocations;
-  }
+class _STOListViewState extends State<STOListView> { 
 
   @override
   void initState() {
     super.initState();
-    //recalculateLocations();
   }
 
   @override
@@ -107,7 +23,7 @@ class _STOListViewState extends State<STOListView> {
             title: Text(
                 AppLocalizations.of(context)!.translate("locations_history"))),
         body: FutureBuilder<List<CustomLocation>>(
-          future: recalculateLocations(),
+          future: CustomLocationsManager.getLocations(25),
           builder: (context, snapshot) {
             if (!snapshot.hasData) {
               return Center(
@@ -142,16 +58,6 @@ class _STOListViewState extends State<STOListView> {
                       ScaffoldMessenger.of(context).showSnackBar(
                           new SnackBar(content: new Text("Location removed")))
                     },
-                    /*confirmDismiss: (DismissDirection direction) async {
-                      //if(direction == DismissDirection.)
-                      // if (direction == left)
-                      await thisLocation.deleteThisLocation();
-                      setState(() {
-                        //recalculateLocations();
-                        //thisLocation = snapshot.data![index];
-                      });
-                      return true;
-                    },*/
                   );
                 });
           },

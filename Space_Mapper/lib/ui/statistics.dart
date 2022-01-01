52e97fdc79821860096369de1857c4f5e03ab05a
pablogalve <pablogalve100@gmail.com>
2022-01-01 18:11:03 +0100

    Design for statistics screen

--- Space_Mapper/lib/ui/statistics.dart
+++ Space_Mapper/lib/ui/statistics.dart
@@ -8,40 +8,82 @@ class MyStatistics extends StatefulWidget {
   _MyStatisticsState createState() => _MyStatisticsState();
 }
 
-class MyChartData {
+class MonthlyContactData {
   final int id;
   int value;
   final DateTime date;
 
-  MyChartData(this.id, this.value, this.date);
+  MonthlyContactData(this.id, this.value, this.date);
+}
+
+class ContactByGenderData {
+  final int
+      gender; //TODO: Change to string. Currently it's -> 0=male, 1=female, 2=other
+  int value = 0;
+
+  ContactByGenderData(this.gender, this.value);
 }
 
 class _MyStatisticsState extends State<MyStatistics>
     with SingleTickerProviderStateMixin {
   final scaffoldKey = GlobalKey<ScaffoldState>();
 
-  List<MyChartData> _myData = [];
+  List<MonthlyContactData> _monthlyContactData = [];
+  List<ContactByGenderData> _contactByGenderData = [];
 
   @override
   void initState() {
     super.initState();
-    getMyData();
+    getMonthlyContactsData();
+    getContactsByGenderData();
   }
 
-  void getMyData() async {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+            bottom: TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.bar_chart)),
+                Tab(icon: Icon(Icons.pie_chart)),
+              ],
+            ),
+            title: Text("Statistics"),
+            leading: IconButton(
+              icon: Icon(
+                Icons.arrow_back,
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            )),
+        body: TabBarView(
+          children: [
+            displayMonthlyContacts(),
+            displayContactsByGender(),
+          ],
+        ),
+      ),
+    ));
+  }
+
+  void getMonthlyContactsData() async {
     List<Contact> contacts = await StorageDatabase.instance.readAllContacts();
     setState(() {
       // Initialize data
       for (int i = 0; i < 12; ++i) {
-        _myData.add(
-            MyChartData(i, 0, DateTime.now().subtract(Duration(days: 30 * i))));
+        _monthlyContactData.add(MonthlyContactData(
+            i, 0, DateTime.now().subtract(Duration(days: 30 * i))));
       }
       // Fill data
       for (int i = 0; i < contacts.length; ++i) {
-        for (int j = 0; j < _myData.length; ++j) {
-          if (_myData[j].date.month == contacts[i].date.month) {
-            if (_myData[j].date.year == contacts[i].date.year) {
-              _myData[j].value++;
+        for (int j = 0; j < _monthlyContactData.length; ++j) {
+          if (_monthlyContactData[j].date.month == contacts[i].date.month) {
+            if (_monthlyContactData[j].date.year == contacts[i].date.year) {
+              _monthlyContactData[j].value++;
               continue;
             }
           }
@@ -50,23 +92,54 @@ class _MyStatisticsState extends State<MyStatistics>
     });
   }
 
-  Widget getHomePage() {
+  void getContactsByGenderData() async {
+    List<Contact> contacts = await StorageDatabase.instance.readAllContacts();
+    setState(() {
+      // Initialize data
+      _contactByGenderData.add(ContactByGenderData(0, 0));
+      _contactByGenderData.add(ContactByGenderData(1, 0));
+      _contactByGenderData.add(ContactByGenderData(2, 0));
+
+      // Fill data
+      for (int i = 0; i < contacts.length; ++i) {
+        int myGender = -1;
+        if (contacts[i].gender == "male")
+          myGender = 0;
+        else if (contacts[i].gender == "female")
+          myGender = 1;
+        else if (contacts[i].gender == "other") myGender = 2;
+
+        for (int j = 0; j < _contactByGenderData.length; ++j) {
+          if (_contactByGenderData[j].gender == myGender) {
+            _contactByGenderData[j].value++;
+            continue;
+          }
+        }
+      }
+    });
+  }
+
+  Widget displayMonthlyContacts() {
     final List<Widget> widgets = <Widget>[];
-    if (_myData.length > 0) {
-      final List<Charts.Series<MyChartData, DateTime>> seriesList = [
-        Charts.Series<MyChartData, DateTime>(
+    if (_monthlyContactData.length > 0) {
+      final List<Charts.Series<MonthlyContactData, DateTime>> seriesList = [
+        Charts.Series<MonthlyContactData, DateTime>(
           id: 'chart000',
-          domainFn: (MyChartData chartData, _) => chartData.date,
-          measureFn: (MyChartData chartData, _) => chartData.value,
-          colorFn: (MyChartData chartData, _) =>
-              Charts.MaterialPalette.deepOrange.shadeDefault,
-          data: _myData,
+          domainFn: (MonthlyContactData chartData, _) => chartData.date,
+          measureFn: (MonthlyContactData chartData, _) => chartData.value,
+          colorFn: (MonthlyContactData chartData, _) =>
+              Charts.MaterialPalette.blue.shadeDefault,
+          data: _monthlyContactData,
         ),
       ];
       final Charts.TimeSeriesChart chart = Charts.TimeSeriesChart(
         seriesList,
         animate: true,
       );
+      widgets.add(Text(
+        "Contacts in the last 12 months",
+        style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+      ));
       widgets.add(Padding(
         padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 12),
         child: SizedBox(
@@ -81,16 +154,39 @@ class _MyStatisticsState extends State<MyStatistics>
     );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      key: scaffoldKey,
-      body: Container(
-        // Home Page
-        color: Theme.of(context).colorScheme.primary,
-        padding: MediaQuery.of(context).padding,
-        child: getHomePage(),
-      ),
+  Widget displayContactsByGender() {
+    final List<Widget> widgets = <Widget>[];
+    if (_contactByGenderData.length > 0) {
+      final List<Charts.Series<ContactByGenderData, int>> seriesList = [
+        Charts.Series<ContactByGenderData, int>(
+          id: 'gender',
+          domainFn: (ContactByGenderData chartData, _) => chartData.gender,
+          measureFn: (ContactByGenderData chartData, _) => chartData.value,
+          colorFn: (ContactByGenderData chartData, _) =>
+              Charts.MaterialPalette.blue.shadeDefault,
+          data: _contactByGenderData,
+        ),
+      ];
+      final Charts.PieChart chart = Charts.PieChart<int>(seriesList,
+          animate: true,
+          defaultRenderer: new Charts.ArcRendererConfig(
+              arcWidth: 60,
+              arcRendererDecorators: [new Charts.ArcLabelDecorator()]));
+      widgets.add(Text(
+        "Contacts by gender",
+        style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+      ));
+      widgets.add(Padding(
+        padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 12),
+        child: SizedBox(
+          height: 250,
+          child: chart,
+        ),
+      ));
+    }
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: widgets,
     );
   }
 }

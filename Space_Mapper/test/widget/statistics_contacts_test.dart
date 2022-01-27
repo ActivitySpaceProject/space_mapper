39ef130323e8cbd391aa0b156d2b986fd36755ed
Pablo Galve <pablogalve100@gmail.com>
2022-01-27 17:16:47 +0100

    [Testing] Unit tests added to surveys

--- Space_Mapper/test/widget/statistics_contacts_test.dart
+++ Space_Mapper/test/widget/statistics_contacts_test.dart
@@ -2,9 +2,6 @@ import 'package:asm/ui/statistics/statistics.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  // Define a test. The TestWidgets function also provides a WidgetTester
-  // to work with. The WidgetTester allows you to build and interact
-  // with widgets in the test environment.
   testWidgets('StatsContacts displays all the stats',
       (WidgetTester tester) async {
     // Create the widget by telling the tester to build it.

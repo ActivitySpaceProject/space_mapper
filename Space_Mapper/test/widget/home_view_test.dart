a12c64fa36004fd19ca5f477163b34c250e1d7d3
Pablo Galve <pablogalve100@gmail.com>
2022-02-04 19:01:36 +0100

    [Testing] Side drawer menu

--- Space_Mapper/test/widget/home_view_test.dart
+++ Space_Mapper/test/widget/home_view_test.dart
@@ -15,4 +15,14 @@ void main() {
     expect(find.byIcon(Icons.gps_fixed), findsOneWidget);
     expect(find.byIcon(Icons.person), findsOneWidget);
   });
+
+  testWidgets('Go to add contact screen', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: HomeView("Space Mapper")));
+
+    final addContactBtn = find.byIcon(Icons.person);
+    await tester.tap(addContactBtn);
+    await tester.pumpAndSettle();
+    
+    expect(find.byType(AppBar), findsOneWidget);
+  });
 }

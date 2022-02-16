1d46f34764575581400a35cb916969c9c627a138
Pablo Galve <pablogalve100@gmail.com>
2022-02-16 16:43:09 +0100

    remove comments

--- Space_Mapper/test_driver/driver.dart
+++ Space_Mapper/test_driver/driver.dart
@@ -1,9 +1,6 @@
 import '../lib/main.dart' as app;
 import 'package:flutter_driver/driver_extension.dart';
 
-/// 集成测试运行 flutter drive --profile --target=test_driver/driver.dart （模拟器去除--profile）
-/// 集成测试暂不支持拖动、长按等方式。 https://github.com/flutter/flutter/issues/27552
-/// 其他问题记录：https://github.com/flutter/flutter/issues/12561
 void main() {
   enableFlutterDriverExtension();
   app.main();

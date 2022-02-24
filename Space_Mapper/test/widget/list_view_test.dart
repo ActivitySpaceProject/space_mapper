b734f84e879c77210ca50db13acca8b527e29c02
Pablo Galve <pablogalve100@gmail.com>
2022-02-24 17:20:50 +0100

    [Testing] list_view

---
+++ Space_Mapper/test/widget/list_view_test.dart
@@ -0,0 +1,31 @@
+import 'package:asm/models/custom_locations.dart';
+import 'package:asm/ui/list_view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('list view has an appBar', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: STOListView()));
+
+    expect(find.byType(AppBar), findsOneWidget);
+  });
+
+  testWidgets(
+      'when list view doesnt have data, it displays a CircularProgressIndicator and then an empty container',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: STOListView()));
+
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+    expect(find.byType(Container), findsOneWidget);
+  });
+
+  testWidgets(
+      'list view displays a ListView.builder when it has at least 1 element',
+      (WidgetTester tester) async {
+    //CustomLocationsManager.createCustomLocation(recordedLocation) // TODO: To test this, we have to use Backgroundgeolocation package to generate a test location
+
+    // Simulate that there is 1 recorded location
+
+    await tester.pumpWidget(MaterialApp(home: STOListView()));
+  });
+}

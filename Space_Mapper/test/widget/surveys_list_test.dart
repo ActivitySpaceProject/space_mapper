3da724f9286a2a432a9af96e4d5f380125f6c800
Pablo Galve <pablogalve100@gmail.com>
2022-03-15 17:50:54 +0100

    Add more tests to surveys_list

--- Space_Mapper/test/widget/surveys_list_test.dart
+++ Space_Mapper/test/widget/surveys_list_test.dart
@@ -1,6 +1,8 @@
 import 'package:asm/ui/surveys_list.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+
 import 'package:mockito/mockito.dart';
 
 class MockBuildContext extends Mock implements BuildContext {}
@@ -34,12 +36,39 @@ void main() {
     await tester.pumpWidget(wrappedWidget);
 
     expect(find.byType(ListView), findsOneWidget);
-  });  
+  });
 
 
-
-  /*testWidgets('survey_list: renderProgressBar', (WidgetTester tester) async {  
+  testWidgets('survey_list: renderProgressBar returns an empty container if(loading==false)', (WidgetTester tester) async {  
     await tester.pumpWidget(
         MaterialApp(home: AvailableSurveysScreen()));
-  });*/
+
+    final AvailableSurveysScreenState myWidgetState = tester.state(find.byType(AvailableSurveysScreen));
+
+    myWidgetState.loading = false;
+    final Widget expectedWidget = myWidgetState.renderProgressBar(_mockContext);    
+    // Since the context is mocked, the widget must be wrapped in a Directionality widget. Avoiding this step will cause an error.
+    final Widget wrappedWidget = Directionality(textDirection: TextDirection.ltr, child: expectedWidget);
+
+    await tester.pumpWidget(wrappedWidget);
+
+    expect(find.byType(Container), findsOneWidget);
+    expect(find.byType(LinearProgressIndicator), findsNothing);
+  });
+
+  testWidgets('survey_list: _listViewItemBuilder onTap navigates to SurveyDetail screen', (WidgetTester tester) async {  
+    await tester.pumpWidget(
+        MaterialApp(home: AvailableSurveysScreen()));
+
+    final AvailableSurveysScreenState myWidgetState = tester.state(find.byType(AvailableSurveysScreen));
+
+    final Widget expectedWidget = myWidgetState.renderListView(_mockContext);
+    
+    // Since the context is mocked, the widget must be wrapped in a Directionality widget. Avoiding this step will cause an error.
+    final Widget wrappedWidget = Directionality(textDirection: TextDirection.ltr, child: expectedWidget);
+
+    await tester.pumpWidget(wrappedWidget);
+
+    expect(find.byType(GestureDetector), findsWidgets);
+  });
 }

bef1c5fce7e1a911e42088be70c001260f8f6708
Pablo Galve <pablogalve100@gmail.com>
2022-06-03 20:18:09 +0200

    Send also os, speed, activity and altitude

--- Space_Mapper/lib/models/send_data_to_api.dart
+++ Space_Mapper/lib/models/send_data_to_api.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'package:flutter_background_geolocation/flutter_background_geolocation.dart'
     as bg;
@@ -8,11 +9,24 @@ class SendDataToAPI {
     final uri =
         Uri.parse('https://testingserver.activityspaceproject.com/api/write');
     final headers = {'Content-Type': 'application/json'};
+
+    String os = 'os not detected';
+    if(Platform.isAndroid) os = 'Android';
+    else if(Platform.isIOS) os = 'iOS';
+
     Map<String, dynamic> body = {
       'user_UUID': '00000000-0000-0000-0000-000000000000',
       'user_code': '00000000',
+      'app_version': '0.0.0',
+      'os': os,
+      'type_of_data': '0000000000',
+      'message': '',      
       'lon': location.coords.longitude,
-      'lat': location.coords.latitude
+      'lat': location.coords.latitude,
+      //'unix_time': 0 /*location.timestamp*/,
+      'speed': location.coords.speed,
+      'activity': location.activity.type,
+      'altitude': location.coords.altitude,
     };
     String jsonBody = json.encode(body);
     final encoding = Encoding.getByName('utf-8');
@@ -30,7 +44,7 @@ class SendDataToAPI {
     if (statusCode == 201) {
       print(responseBody);
     } else {
-      print("request failed");
+      print("----- API request failed -----");
     }
   }
 }
